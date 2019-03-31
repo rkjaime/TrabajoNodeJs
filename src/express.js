34a30e49943b009c6d411cb0c9bcefb07c5faf47
Jaime Mejia <jdmejiaq@unal.edu.co>
2019-03-30 19:27:01 -0500

    archivos de instalacion para los handlebars,nodemon y finalmente archivos iniciales para el trababajo

--- src/express.js
+++ src/express.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const hbs = require('hbs');
 const directoriopublico = path.join(__dirname,'../public' );
 app.use(express.static(directoriopublico));
+app.set('view engine','hbs');
+app.get('/',(req,res) =>{
+	res.render('index',{
+		estudiante:'sebastian'});
+});
+
+app.get('/calculos',(req,res)=>{
+	res.render('calculos',{
+		estudiante:'Juliana'
+	});
+});
 //app.use(express.static(__dirname + '/public'))
 console.log(__dirname);
 app.listen(4000,()=>{
