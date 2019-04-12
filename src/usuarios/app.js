f025d0e3c7e49178a83092a054f650182e9fe847
Jaime Mejia <jdmejiaq@unal.edu.co>
2019-04-12 14:49:52 -0500

    siguiendo cambios

--- src/usuarios/app.js
+++ src/usuarios/app.js
@@ -365,7 +365,7 @@ MongoClient.connect(uri, function(err, client) {
         console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
    }
    console.log('Connected...');
-   const collection = client.db("test").collection("devices");
+   const collection = client.db("asignaturas").collection("aspirantes");
    // perform actions on the collection object
    client.close();
 });
