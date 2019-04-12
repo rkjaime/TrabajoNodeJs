518c8810165418d6d45b250483d57663b84981b4
Jaime Mejia <jdmejiaq@unal.edu.co>
2019-04-12 10:06:06 -0500

    probando

--- src/usuarios/app.js
+++ src/usuarios/app.js
@@ -8,7 +8,7 @@ const Estudiante = require('../models/estudiantes');
 const Aspirante = require('../models/CursosUsuarios');
 const Cursos = require('../models/cursos');
 const port = process.env.PORT || 3000;
-const process.env.urldb ='mongodb://localhost:27017/asignaturas';
+process.env.urldb ='mongodb://localhost:27017/asignaturas';
 const session = require('express-session');
 require('./helpers');
 const directoriopublico = path.join(__dirname,'../../public' );
