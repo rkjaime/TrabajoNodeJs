03588093a16ea6a46965dbe7c244cf78b534b45b
Jaime Mejia <jdmejiaq@unal.edu.co>
2019-04-12 16:09:12 -0500

    sdasda

--- src/usuarios/app.js
+++ src/usuarios/app.js
@@ -351,6 +351,7 @@ const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://123:123@nodejstdea-m1nj0.mongodb.net/test?retryWrites=true";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 client.connect(err => {
+    console.log(err)
   const collection = client.db("test").collection("devices");
   // perform actions on the collection object
   client.close();
